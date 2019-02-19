5e5401312699da24b6de637351a8c58b848e3d03
danish09 <danishsiddiqui09@gmail.com>
2019-02-19 14:08:29 +0000

    updated region

--- node-app.tf
+++ node-app.tf
@@ -4,7 +4,7 @@ variable "secret_key" {}
 provider "aws" {
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
-  region     = "us-east-1"
+  region     = "eu-west-1"
 }
 
 data "aws_ami" "node_app_ami" {
